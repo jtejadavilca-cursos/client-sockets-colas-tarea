d23185a3bc43ab1407ca679af4e13f9f2f41bb17
Jose Tejada <jtejadavilca@gmail.com>
2020-07-07 02:24:51 -0500

    Avance parte cliente sistema colas

--- src/app/pages/publico/publico.component.ts
+++ src/app/pages/publico/publico.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import Ticket from '../../classes/ticket';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-publico',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublicoComponent implements OnInit {
 
-  constructor() { }
+  private API_COLA = environment.APIS.API_COLAS;
 
-  ngOnInit(): void {
+  lstCuatroUltimosAtendidos: Ticket[] = [];
+
+  constructor(
+    private http: HttpClient
+  ) {
   }
 
+  ngOnInit(): void {
+    this.obtenerCuatroUltimos();
+  }
+
+  obtenerCuatroUltimos(): void {
+    this.http.get<Ticket[]>(`${ this.API_COLA }/cuatro-ultimos`)
+          .subscribe( cu => this.lstCuatroUltimosAtendidos = cu );
+  }
 }
